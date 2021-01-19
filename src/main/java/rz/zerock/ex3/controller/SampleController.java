57723ae5496a3e8b861a0226a49ac1f7c12996b2
hyun-cho <hyun__cho@naver.com>
2021-01-20 00:25:51 +0900

    구현완료 chapter 3.1 #3 helloworld 페이지 구현

--- src/main/java/rz/zerock/ex3/controller/SampleController.java
+++ src/main/java/rz/zerock/ex3/controller/SampleController.java
@@ -1,2 +1,20 @@
-package rz.zerock.ex3.controller;public class SampleController {
+package rz.zerock.ex3.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/*
+* @Log4j2 의 경우에 Lombok의 기능으로 스프링 부트가 로그 라이브러리 중 스프링부트에서 적용가능
+* */
+@Controller
+@RequestMapping("/sample")
+@Log4j2
+public class SampleController {
+    @GetMapping("/ex1")
+    public void ex1() {
+        log.info("ex1....");
+    }
 }
